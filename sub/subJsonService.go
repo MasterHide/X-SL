fb9638dc275245db20a5182ef2be6f499b48cc4d
x404 MASTER <71309968+MasterHide@users.noreply.github.com>
2025-02-22 00:20:41 +0530

    Update subJsonService.go

--- sub/subJsonService.go
+++ sub/subJsonService.go
@@ -6,12 +6,12 @@ import (
 	"fmt"
 	"strings"
 
-	"x-sl/database/model"
-	"x-sl/logger"
-	"x-sl/util/json_util"
-	"x-sl/util/random"
-	"x-sl/web/service"
-	"x-sl/xray"
+	"x-ui/database/model"
+	"x-ui/logger"
+	"x-ui/util/json_util"
+	"x-ui/util/random"
+	"x-ui/web/service"
+	"x-ui/xray"
 )
 
 //go:embed default.json
